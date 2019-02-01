0d7159c2cce9487b32962960a2a0f7e0bde11817
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2019-02-01 20:22:37 +0530

    Mongo DB addition

--- mfappnew/server/index.js
+++ mfappnew/server/index.js
@@ -1,7 +1,26 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-var port = 3000;
+
+const app = express();
+
+const port = 3000;
+const config = require('./config/database');
+
+//Set up default mongoose connection
+mongoose.connect(config.database,{ useNewUrlParser: true });
+
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 
 app.get("/", (req, res) => {
     res.send("Please pass the correct route");
