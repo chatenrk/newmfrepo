84e20b770f87cfaf4027bdd966008b3f758b736b
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2019-02-01 20:03:02 +0530

    Basic App route

--- mfappnew/server/index.js
+++ mfappnew/server/index.js
@@ -1,5 +1,11 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+
+var port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Please pass the correct route");
+});
 
 app.listen(port);
 console.log("Server running on port 3000");
