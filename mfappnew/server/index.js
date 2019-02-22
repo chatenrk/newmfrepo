fd4c799f458ec9d8ff49a9edf2c29490d4bdd086
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2019-02-22 18:20:41 +0530

    update

--- mfappnew/server/index.js
+++ mfappnew/server/index.js
@@ -1,29 +1,38 @@
 const express = require("express");
-const path = require('path');
-const mongoose = require('mongoose');
-const passport = require('passport');
-
+const path = require("path");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cors = require('cors');
 
 const app = express();
 
 const port = 3000;
-const config = require('./config/database');
+const config = require("./config/database");
 
 //Set up default mongoose connection
-mongoose.connect(config.database,{ useNewUrlParser: true });
+mongoose.connect(config.database, { useNewUrlParser: true });
 
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
 var db = mongoose.connection;
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// Log DB Connection success message
+db.on("connected", () => {
+  console.log("Connected to DB " + config.database);
+});
 
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.get("/", (req, res) => {
-    res.send("Please pass the correct route");
+  res.send("Please pass the correct route");
 });
 
 app.listen(port);
