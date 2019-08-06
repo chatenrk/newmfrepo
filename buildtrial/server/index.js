fd5e3978b7411dae5d630adeff6e65f3a06ee469
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2019-08-06 22:17:00 +0100

    Simple Html Display

--- buildtrial/server/index.js
+++ buildtrial/server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+
 const app = express();
 
 const port = 3000;
 
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname,'../client')));
 app.use('/', (req,res)=>{
-    res.send("Hello world");
+    res.send("Please pass the correct route");
 })
 
 app.listen(port);
