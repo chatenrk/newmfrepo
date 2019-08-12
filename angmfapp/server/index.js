7ab52999a9facf711b09494e1522ff9d28dd9864
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2019-08-12 18:56:02 +0100

    Initial Commit

---
+++ angmfapp/server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const port = 3000;
+
+
+app.use(cors());
+app.use(express.static(path.join(__dirname,'../client')));
+
+app.get('/', function(req,res){
+	res.send('Please pass the correct route');
+});
+
+app.listen(port);
+console.log("listening on port 3000");
