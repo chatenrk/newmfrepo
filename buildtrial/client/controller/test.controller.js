2eda45ae3616c5085e47601a01b146c61ed34506
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2019-08-12 18:17:13 +0100

    Update Build

---
+++ buildtrial/client/controller/test.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  function(BaseController, History) {
+    "use strict";
+
+    return BaseController.extend("build_test.controller.test", {});
+  }
+);
